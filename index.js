1b670eda98e39de912c5a04238be2e4877c08866
Amritesh VB <amritesh.amr005@gmail.com>
2023-07-24 00:06:56 +0530

    fourth-commit

--- index.js
+++ index.js
@@ -1,106 +1,52 @@
+let playerOption;
+let cpuOption;
 
-
-function rockMessage(){
-    let a = document.getElementById("response")
-    let res = document.getElementById("result")
-    let playerOption = "rock"
-    let msg = ""
-    a.textContent = "You Selected Rock"
-    cpuOption = chooseCpu()
-    if (playerOption === cpuOption){
-        msg = "Draw"
-    }
-    else{
-        let sc = compareRock(playerOption, cpuOption)
-        if (sc === 1){
-            msg = "Win"
-        }
-        else{
-            msg = "Lose"
-        }
-    }
-    res.textContent = `Result : ${msg}`
-
-
+function setPlayerOption(option){
+    let message = document.getElementById("response");
+    playerOption = option;
+    message.textContent = `Player has selected ${option}`
 }
 
-function paperMessage(){
-    let a = document.getElementById("response")
+
+function playGame(){
+    player = document.getElementById("response");
+    opponent = document.getElementById("opponent");
+    result = document.getElementById("result");
+    if (!playerOption){
+        player.textContent = "Please select an Option"
+    }else{
+        cpuOption = getCpuOption();
+        opponent.textContent = `CPU has selected ${cpuOption}`
+        if ((playerOption == "rock" && cpuOption == "paper") || (playerOption == "paper" && cpuOption == "scissors") || (playerOption == "scissors" && cpuOption == "rock")){
+            result.textContent = "You Lose"
+        }
+        else if ((playerOption == "rock" && cpuOption == "scissors") || (playerOption == "paper" && cpuOption == "rock") || (playerOption == "scissors" && cpuOption == "paper")){
+            result.textContent = "You Win"
+        }
+        else{
+            result.textContent = "Its a tie !!"
+        }
+    }
     
-    let res = document.getElementById("result")   
-    let playerOption = "paper"
-    let msg = ""
-    a.textContent = "You Selected Paper"
-    cpuOption = chooseCpu()
-    if (playerOption === cpuOption){
-        msg = "Draw"
-    }
-    else{
-        let sc = comparePaper(cpuOption)
-        if (sc === 1){
-            msg = "Win"
-        }
-        else{
-            msg = "Lose"
-        }
-    }
-    res.textContent = `Result : ${msg}`
+
 }
 
-function scissorsMessage(){
-    let a = document.getElementById("response")
-    
-    let res = document.getElementById("result")
-    let playerOption = "scissors"
-    let msg = ""
-    a.textContent = "You Selected Scissors"
-    cpuOption = chooseCpu()
-    if (playerOption === cpuOption){
-        msg = "Draw"
-    }
-    else{
-        let sc = compareScissors(cpuOption)
-        if (sc === 1){
-            msg = "Win"
-        }
-        else{
-            msg = "Lose"
-        }
-    }
-    res.textContent = `Result : ${msg}`
+function getCpuOption(){
+    const availableOptions = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random()*3);
+    return availableOptions[randomIndex];
 }
 
-function chooseCpu(){
-    let op = document.getElementById("opponent")
-    let optionsArray = ["rock", "paper", "scissors"]
-    let randomIndex = Math.floor(Math.random()*3)
-    op.textContent = "CPU selected " + optionsArray[randomIndex]
-    return optionsArray[randomIndex]
-}
 
-function compareRock(option){
-    if (option === "scissors"){
-        return 1
-    }
-    else if (option === "paper"){
-        return 0
-    }
-}
 
-function comparePaper(option){
-    if (option === "rock"){
-        return 1
-    }
-    else if (option === "scissors"){
-        return 0
-    }
-}
-
-function compareScissors(option){
-    if (option === "paper"){
-        return 1
-    }
-    else if (option === "rock"){
-        return 0
-    }
-}
+document.getElementById("rock").addEventListener("click", function(){
+    setPlayerOption("rock");
+    console.log("works");
+});
+document.getElementById("paper").addEventListener("click", function(){
+    setPlayerOption("paper");
+});
+document.getElementById("scissors").addEventListener("click", function(){
+    setPlayerOption("scissors");
+});
+document.getElementById("playButton").addEventListener("click", playGame);
